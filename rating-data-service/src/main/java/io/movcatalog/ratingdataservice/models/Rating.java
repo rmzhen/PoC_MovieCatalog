4fd473af58a6812a6171c4cdd0bb643f119caad2
Calvin Zhen <418224@student.fontys.nl>
2022-09-14 15:57:49 +0200

    10 - Coding the third service and discussing communication

---
+++ rating-data-service/src/main/java/io/movcatalog/ratingdataservice/models/Rating.java
@@ -0,0 +1,28 @@
+package io.movcatalog.ratingdataservice.models;
+
+public class Rating {
+    
+    private String movieId;
+    private int rating;
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return this.movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
