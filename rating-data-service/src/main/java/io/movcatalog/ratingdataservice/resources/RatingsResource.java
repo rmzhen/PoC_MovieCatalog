58e64b20109292b5e2c2b854bc8b5eaa5645b243
Calvin Zhen <418224@student.fontys.nl>
2022-09-18 17:10:38 +0200

    14 - Making a call to the Ratings API

--- rating-data-service/src/main/java/io/movcatalog/ratingdataservice/resources/RatingsResource.java
+++ rating-data-service/src/main/java/io/movcatalog/ratingdataservice/resources/RatingsResource.java
@@ -1,5 +1,9 @@
 package io.movcatalog.ratingdataservice.resources;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,15 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
+        );
+        return ratings;
     }
 
 }
